4018dce039505132120d805199012fc9242c7aeb
Mustafa YILDIZ <mashadow@outlook.com>
2026-01-11 14:49:20 +0300

    refactor: add shared domain and infrastructure layer
    - Add base entity and value object classes
    - Add domain exception handling
    - Move Prisma service to shared infrastructure

---
+++ src/shared/domain/base.entity.ts
@@ -0,0 +1,25 @@
+export abstract class BaseEntity<T> {
+  protected readonly _id: string;
+  protected readonly props: T;
+
+  constructor(id: string, props: T) {
+    this._id = id;
+    this.props = props;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  public equals(entity?: BaseEntity<T>): boolean {
+    if (entity === null || entity === undefined) {
+      return false;
+    }
+
+    if (this === entity) {
+      return true;
+    }
+
+    return this._id === entity._id;
+  }
+}
---
+++ src/shared/domain/index.ts
@@ -0,0 +1,3 @@
+export * from './base.entity';
+export * from './base.value-object';
+export * from './domain.exception';
